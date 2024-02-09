df000770833a4aa9d693a7fd90f09e444c15dead
Sai-Shankar <saishankar2803@gmail.com>
2024-02-10 00:37:20 +0530

    Update cookie_converter.py

--- cookie_converter.py
+++ cookie_converter.py
@@ -4,11 +4,9 @@ import random
 import shutil
 import progressbar
 
-
 try:
     if os.name == "posix":
         folder_path = "cookies"
-
     else:
         while True:
             import tkinter
@@ -21,15 +19,14 @@ try:
                 print("Trying to use default folder 'cookies'\n")
                 folder_path = "cookies"
                 break
-
             else:
                 break
 
     def maximum():
         count = 0
-        for files in os.walk(folder_path):
+        for _, _, files in os.walk(folder_path):
             count += len(files)
-            return count
+        return count
 
     rand_number = random.randint(1, 99999)
 
@@ -57,9 +54,10 @@ try:
 
     path = "json_cookies"
     try:
-        os.mkdir(path)
+        os.makedirs(path, exist_ok=True)
         print(f"Folder {path} created!\n")
         try:
+            pbar.start()  # Start the progress bar
             for filename in os.listdir(folder_path):
                 filepath = os.path.join(folder_path, filename)
                 if os.path.isfile(filepath):
@@ -68,30 +66,23 @@ try:
 
                     json_data = convert_netscape_cookie_to_json(content)
 
-                    with open(f"json_cookies/{filename}", "w", encoding="utf-8") as f:
+                    with open(os.path.join(path, filename), "w", encoding="utf-8") as f:
                         f.write(json_data)
                         print(f"{filename} - DONE!")
-                        if 0 <= progress <= maximum():
-                            pbar.start()
-                            pbar.update(progress)
-                            progress += 1
-                        else:
-                            pass
+                        progress += 1
+                        pbar.update(progress)
+
         except FileNotFoundError:
             print(
-                "Error Occurred :Default 'cookies' folder not found, please select a valid folder"
+                "Error Occurred: Default 'cookies' folder not found, please select a valid folder"
             )
-            os.rmdir(path)
+            shutil.rmtree(path)
 
     except FileExistsError:
-        if (
-            input(
-                "Do you want to remove old cookies folder? (y/n)\n [y] Recommended \n > : "
-            )
-            == "y"
-        ):
+        if input("Do you want to remove old cookies folder? (y/n)\n[y] Recommended\n> : ") == "y":
             shutil.rmtree(path)
-            os.mkdir(path)
+            os.makedirs(path, exist_ok=True)
+            pbar.start()  # Start the progress bar
             for filename in os.listdir(folder_path):
                 filepath = os.path.join(folder_path, filename)
                 if os.path.isfile(filepath):
@@ -100,18 +91,14 @@ try:
 
                     json_data = convert_netscape_cookie_to_json(content)
 
-                    with open(f"json_cookies/{filename}", "w", encoding="utf-8") as f:
+                    with open(os.path.join(path, filename), "w", encoding="utf-8") as f:
                         f.write(json_data)
                         print(f"{filename} - DONE!")
-                        if 0 <= progress <= maximum():
-                            pbar.start()
-                            pbar.update(progress)
-                            progress += 1
-                        else:
-                            pass
-
+                        progress += 1
+                        pbar.update(progress)
         else:
-            os.mkdir(str(f"temp {rand_number}"))
+            os.makedirs(f"temp {rand_number}", exist_ok=True)
+            pbar.start()  # Start the progress bar
             for filename in os.listdir(folder_path):
                 filepath = os.path.join(folder_path, filename)
                 if os.path.isfile(filepath):
@@ -121,18 +108,17 @@ try:
                     json_data = convert_netscape_cookie_to_json(content)
 
                     with open(
-                        f"temp {rand_number}/{filename}", "w", encoding="utf-8"
+                        os.path.join(f"temp {rand_number}", filename),
+                        "w",
+                        encoding="utf-8",
                     ) as f:
                         f.write(json_data)
                         print(f"{filename} - DONE!")
-                        if 0 <= progress <= maximum():
-                            pbar.start()
-                            pbar.update(progress)
-                            progress += 1
-                        else:
-                            pass
+                        progress += 1
+                        pbar.update(progress)
 
-            print(f"\n\nsaved cookies to the temp folder - temp {rand_number}")
+            print(f"\n\nSaved cookies to the temp folder - temp {rand_number}")
+
     pbar.finish()
 
 except KeyboardInterrupt:
